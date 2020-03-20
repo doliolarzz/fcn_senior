a9405fdb6fae33398bb8ff557e7358ed56da9ba7
Warit Prasartritha <doliolarzz@hotmail.com>
2020-03-21 00:37:57 +0700

    fix

--- models/mrunet3D/model.py
+++ models/mrunet3D/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from models.mrunet.unet_backbone import UNet
+from models.mrunet3D.unet3d_backbone import UNet3D
 from global_config import global_config
 import cv2
 
@@ -13,28 +13,28 @@ class MRUNet(nn.Module):
 
         self.h, self.w = config['IN_HEIGHT'], config['IN_WIDTH']
         self.config = config
-        self.backbone = UNet(n_channels=config['IN_LEN']*3 + 2)#.to(self.config['DEVICE'])
-        self.geo = nn.Parameter(data=torch.randn(1, geo_size, self.h, self.w), requires_grad=True)#.to(self.config['DEVICE'])
+        self.backbone = UNet3D(in_channels=3, out_channels=1, final_sigmoid=False, num_levels=4, is_segmentation=False)
+        self.geo = nn.Parameter(data=torch.randn(1, geo_size, 1, self.h, self.w), requires_grad=True)
         self.state_weight = state_weight
-        self.outConv = nn.Conv2d(64, 1, kernel_size=1)
+        out_size = self.backbone.get_out_size()
+        self.outConv = nn.Conv3d(out_size, 1, kernel_size=1)
 
     def get_optFlow(self, input):
         input = input.detach().cpu().numpy()
-        opt = np.zeros((input.shape[0], 2*self.config['IN_LEN']-2, self.h, self.w), dtype=np.float32)
+        opt = np.zeros((input.shape[0], 1, 2*self.config['IN_LEN']-2, self.h, self.w), dtype=np.float32)
         for b in range(input.shape[0]):
-            for i in range(input.shape[1] - 1):
+            for i in range(input.shape[2] - 1):
                 delta = cv2.DISOpticalFlow_create(cv2.DISOPTICAL_FLOW_PRESET_MEDIUM) \
-                    .calc((input[b, i]*255).astype(np.uint8), (input[b, i+1]*255).astype(np.uint8), None)
-                opt[b, i*2] = delta[...,0]
-                opt[b, i*2+1] = delta[...,1]
-        return torch.autograd.Variable(data=torch.from_numpy(opt).float(), requires_grad=False).cuda()#.to(self.config['DEVICE'])
+                    .calc((input[b, 0, i]*255).astype(np.uint8), (input[b, 0, i+1]*255).astype(np.uint8), None)
+                opt[b, 0, i*2] = delta[...,0]
+                opt[b, 0, i*2+1] = delta[...,1]
+        return torch.autograd.Variable(data=torch.from_numpy(opt).float(), requires_grad=False).cuda()
 
     def get_next_state(self, next_input, prev_state):
 
         optFlow = self.get_optFlow(next_input)
         next_state = self.state_weight * optFlow + (1 - self.state_weight) * prev_state
         return next_state
-
     
     def forward(self, input):
 
@@ -42,10 +42,10 @@ class MRUNet(nn.Module):
         cur_input = input
         cur_state = self.get_optFlow(cur_input)
         for i in range(self.config['OUT_LEN']):
-
-            cur_input = cur_input.cuda()#.to(self.config['DEVICE'])
-            cur_state = cur_state.cuda()#.to(self.config['DEVICE'])
-            x = torch.cat([cur_input, cur_state, self.geo.expand(cur_input.shape[0], -1, -1, -1)], 1)
+            
+            cur_input = cur_input[:, :, 1:].cuda()
+            cur_state = cur_state.cuda()
+            x = torch.cat([cur_input, cur_state, self.geo.expand(cur_input.shape[0], -1, cur_input.shape[2], -1, -1)], 1)
 
             self.backbone = self.backbone
             x = self.backbone(x)
@@ -57,9 +57,6 @@ class MRUNet(nn.Module):
             cur_input = torch.cat([cur_input[:, 1:], x], 1)
             cur_state = self.get_next_state(cur_input, cur_state)
 
-        # for i in range(len(outputs)):
-        #     print(outputs[i].device, outputs[i].size())
-
-        return torch.cat(outputs, 1)#outputs#torch.cat([o.cuda(dev) for o in outputs], 1)#.cuda(dev)
+        return torch.cat(outputs, 1)
 
 
